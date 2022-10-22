16affd580ea2883644cf7d456bf358fabba44296
RogerFranck <RogerAlmeydaRamos@outlook.com>
2022-10-22 21:43:56 +0000

    Deploying to gh-pages from @ RogerFranck/UnoBots@40f85074a9516d72efec0f061f00f5a62751a720 🚀

--- _next/static/QsinRVsridprZ2l8IH_6l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f4fa0c74335e838a.css","static/chunks/pages/index-5ff9c418fc8a89fd.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
