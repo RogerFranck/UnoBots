8a016149ae3f19dd6d8d7ee393cb4f6ef506c695
RogerFranck <RogerAlmeydaRamos@outlook.com>
2022-10-24 13:08:14 -0500

    main deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,  
+  images: {
+    loader: "akamai",
+    path: ""
+  },
+  basePath: "/UnoBots",
+  assetPrefix: "/UnoBots"
 }
 
 module.exports = nextConfig
